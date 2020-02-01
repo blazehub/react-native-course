f430a19a2c4ebab88e020318fd163e5010686114
blazehub <blaze.avinash@gmail.com>
2020-02-01 10:38:28 +0530

    scrollview flatlist

--- App.js
+++ App.js
@@ -1,17 +1,34 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
 
   const [enteredGoal, setEnteredGoal] = useState('');
-  const [courseGoals, setCourseGoals] = useState([]);
+  const [courseGoals, setCourseGoals] = useState([
+    { key: Math.random().toString(), value: 'Learn React Native' },
+    { key: Math.random().toString(), value: 'Learn React Native' },
+    { key: Math.random().toString(), value: 'Learn React Native' },
+    { key: Math.random().toString(), value: 'Learn React Native' },
+    { key: Math.random().toString(), value: 'Learn React Native' },
+    { key: Math.random().toString(), value: 'Learn React Native' },
+    { key: Math.random().toString(), value: 'Learn React Native' },
+    { key: Math.random().toString(), value: 'Learn React Native' },
+    { key: Math.random().toString(), value: 'Learn React Native' },
+    { key: Math.random().toString(), value: 'Learn React Native' },
+    { key: Math.random().toString(), value: 'Learn React Native' },
+    { key: Math.random().toString(), value: 'Learn React Native' },
+    { key: Math.random().toString(), value: 'Learn React Native' },
+    { key: Math.random().toString(), value: 'Learn React Native' },
+  ]);
 
   const goalInputHandler = (enteredText) => {
     setEnteredGoal(enteredText);
   }
 
   const addGoalHandler = () => {
-    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+    setCourseGoals(currentGoals => [...currentGoals,
+    { key: Math.random().toString(), value: enteredGoal }
+    ]);
   }
 
   return (
@@ -23,9 +40,12 @@ export default function App() {
           value={enteredGoal} />
         <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <View>
-
-      </View>
+      <FlatList data={courseGoals} renderItem={itemData =>
+        <View style={styles.listItem}>
+          <Text>{itemData.item.value}</Text>
+        </View>
+      }>
+      </FlatList>
     </View>
   );
 }
@@ -43,6 +63,13 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 5,
     flex: 1
+  },
+  listItem: {
+    marginVertical: 10,
+    padding: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
   }
 });
 
